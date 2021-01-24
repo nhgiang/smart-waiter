86dff13df71996b8b21e1ecf4c7cc01ce03fd21f
nhgiang <hgiang2000@gmail.com>
2021-01-25 00:13:23 +0700

    report

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -9,6 +9,7 @@ import { sumBy } from 'lodash';
   styleUrls: ['./bill.component.scss']
 })
 export class BillComponent implements OnInit {
+  totalPrice = 0;
   public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
@@ -26,7 +27,7 @@ export class BillComponent implements OnInit {
   public barChartPlugins = [];
 
   public barChartData: ChartDataSets[] = [
-    { data: [], label: 'Series A' },
+    { data: [0, 1, 2], label: 'Series A' },
   ];
   salesReport = 'day';
   orders: any[];
@@ -39,17 +40,83 @@ export class BillComponent implements OnInit {
   fetch() {
     this.admin.order.getOrders('').subscribe(res => {
       this.orders = res;
-      this.orders.forEach(order => {
-        if (new Date(order.createdDate).toLocaleDateString() === new Date().toLocaleDateString()) {
-          if (new Date(order.createdDate).) {
-
+      // tslint:disable-next-line: max-line-length
+      const orderByDay = this.orders.filter(t => new Date(t.createdDate).toISOString().split('T')[0] === new Date('2021-01-23T03:13:00.835').toISOString().split('T')[0]);
+      this.totalPrice = sumBy(orderByDay, (t) => {
+        return t.totalPrice;
+      });
+      for (let i = 0; i <= 11; i++) {
+        // if (new Date(order.createdDate).getUTCHours() === (i + 10)) {
+        this.barChartData[0].data[i] = sumBy(orderByDay, (t) => {
+          if (new Date(t.createdDate).getUTCHours() === (i + 10)) {
+            return t.totalPrice;
+          } else {
+            return 0;
           }
-        }
-      })
+        });
+      }
     });
+
   }
 
   filterReport(event) {
+    if (event.target.value === 'week') {
+      this.barChartLabels = ['T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'CN']
+   
+      const orderByWeek = this.orders.filter(t => {
+        // tslint:disable-next-line: max-line-length
+        if (this.getMonday(new Date()).getFullYear() === new Date(t.createdDate).getFullYear()
+          && this.getMonday(new Date()).getMonth() === new Date(t.createdDate).getMonth()) {
+          if (this.getMonday(new Date()).getUTCDate() <= new Date(t.createdDate).getUTCDate() &&
+            new Date(t.createdDate).getUTCDate() <= (this.getMonday(new Date()).getUTCDate() + 6)) {
+            return true;
+          }
+        }
+      })
+      this.totalPrice = sumBy(orderByWeek, (t) => {
+        return t.totalPrice;
+      });
+      for (let i = 0; i <= 6; i++) {
+        // if (new Date(order.createdDate).getUTCHours() === (i + 10)) {
+        this.barChartData[0].data[i] = sumBy(orderByWeek, (t) => {
+          if (new Date(t.createdDate).getUTCDay() === (i + 1) && i !== 6) {
+            return t.totalPrice;
+          } else if (i === 6 && new Date(t.createdDate).getUTCDay() === 0) {
+            return t.totalPrice;
+          } else {
+            return 0;
+          }
+        });
+      }
+    } else if (event.target.value === 'day') {
 
+      // tslint:disable-next-line: max-line-length
+      const orderByDay = this.orders.filter(t => new Date(t.createdDate).toISOString().split('T')[0] === new Date('2021-01-23T03:13:00.835').toISOString().split('T')[0]);
+      this.totalPrice = sumBy(orderByDay, (t) => {
+        return t.totalPrice;
+      });
+      for (let i = 0; i <= 11; i++) {
+        // if (new Date(order.createdDate).getUTCHours() === (i + 10)) {
+        this.barChartData[0].data[i] = sumBy(orderByDay, (t) => {
+          if (new Date(t.createdDate).getUTCHours() === (i + 10)) {
+            return t.totalPrice;
+          } else {
+            return 0;
+          }
+        });
+      }
+    } else if (event.target.value === 'month') {
+
+
+    } else {
+
+    }
+  }
+
+  getMonday(d) {
+    d = new Date(d);
+    const day = d.getDay();
+    const diff = d.getDate() - day + (day === 0 ? -6 : 1);
+    return new Date(d.setDate(diff));
   }
 }
