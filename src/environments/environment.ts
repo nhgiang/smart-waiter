78b99507242bbb1c3593c75b0965e98e46dcdc33
nhgiang <hgiang2000@gmail.com>
2021-01-25 00:14:29 +0700

    config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://104.131.36.144:9999',
+  apiUrl: 'https://104.131.36.144:9999',
   localStorageKey: 'data',
-  socketEnpoint: 'http://104.131.36.144:9999/ws'
+  socketEnpoint: 'https://104.131.36.144:9999/ws'
 };
 
 /*
