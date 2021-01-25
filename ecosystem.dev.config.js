a17229196526fc553b7c836137ea1941df33d427
ninhdoan <ninhdoanhai@vccorp.vn>
2021-01-25 12:15:49 +0700

    add pm2 config + update server.js

---
+++ ecosystem.dev.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "apps": [
+        {
+            "name": "Document",
+            "script": "./server/app.js",
+            "watch": ["server"],
+            "ignore_watch": ["logs", "node_modules"],
+            "env": {
+                "NODE_ENV": "development",
+            },
+            "log_date_format": "YYYY-MM-DD HH:mm:ss",
+            "error_file": "./logs/pm2.err",
+            "out_file": "./logs/pm2.log",
+        }
+    ]
+
+};
--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const app = express()
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/smart-waiter'))
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
 
-res.sendFile(path.join(__dirname + '/dist/smart-waiter/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/smart-waiter/index.html'))
 })
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080)
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`App listening on port 8000!`)
+});
